27426063a86d889f4a250a0494fe9a6342d1c44f
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-10-13 12:07:36 -0300

    creating card products

---
+++ src/api/fetchProducts.js
@@ -0,0 +1,15 @@
+export const fetchProducts = (query) => {
+	return new Promise((resolve, reject) => {
+		fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+			.then(response => {
+				if (!response.ok) {
+					reject({
+						status: response.status,
+						statusText: response.statusText
+					});
+				} else return response.json();
+			})
+			.then(data => resolve(data.results))
+			.catch(err => reject(err));
+	});
+};
