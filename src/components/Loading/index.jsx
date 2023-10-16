3badfef7da07cec3842ec818ed829a853bf2b0e7
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-10-16 15:38:17 -0300

    loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,9 @@
+import './Loading.css';
+
+import React from 'react';
+import { BiLoaderAlt } from 'react-icons/bi';
+
+export const Loading = () => {
+	return <BiLoaderAlt className="loading" />;
+};
+
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -4,20 +4,24 @@ import React, { useState, useEffect } from 'react';
 /** @name Internal */
 import { fetchProducts } from '../../api/fetchProducts';
 import { ProductCard } from '../ProductCard';
+import { Loading } from '../Loading';
 
 export const Products = () => {
 
 	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		fetchProducts('iphones').then((response) => {
 			setProducts(response);
+			setLoading(false);
 		});
 	}, []);
 
 	return (
-		<section className="products container">
-			{products.map(product => <ProductCard key={product.id} data={product} />)}
-		</section>
+		(loading && <Loading />) || (
+			<section className="products container">
+				{products.map(product => <ProductCard key={product.id} data={product} />)}
+			</section>)
 	);
 };
